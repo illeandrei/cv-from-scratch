483d274d057b2ba27c3af92db8a2e6a677596883
illeandrei <ille.andrei.v@gmail.com>
2018-12-08 15:28:10 +0200

    append basic list to skills page

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 console.log('script loaded');
 
 function initSkillsPage() {
-    $.ajax('data/skills.json').done(function (data) {
-        console.log('data: ', data);
+    $.ajax('data/skills.json').done(function (skills) {
+        console.log('skills: ', skills);
+
+        var resultList = $('#skills-page ul');
+
+        var listItems = skills.map(function (skill) {
+            return `<li>${skill.name.toUpperCase()}</li>`;
+        });
+
+        resultList.append(listItems.join(''));
     })
 }
 
