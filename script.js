158c34ad2072ab28ca07b8c0b14e9c5a79e0392a
illeandrei <ille.andrei.v@gmail.com>
2018-12-08 15:40:57 +0200

    removed unnecessary logs

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-console.log('script loaded');
-
 function initSkillsPage() {
     $.ajax('data/skills.json').done(function (skills) {
-        console.log('skills: ', skills);
-
         var resultList = $('#skills-page ul');
 
         var listItems = skills.map(function (skill) {
