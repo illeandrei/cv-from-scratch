86d946f3b2b6268b548f2b8157d7f34f210382a7
illeandrei <ille.andrei.v@gmail.com>
2018-12-08 15:17:35 +0200

    load skills data from json with ajax

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 console.log('script loaded');
 
+function initSkillsPage() {
+    $.ajax('data/skills.json').done(function (data) {
+        console.log('data: ', data);
+    })
+}
+
 $('#skills-page').show();
+initSkillsPage();
